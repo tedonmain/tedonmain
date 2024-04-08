76c951cd9093966ec1ed536050be0c437b3e0f77
Ted Finley <tedonmain@gmail.com>
2024-04-08 13:39:47 -0700

    Day 001

---
+++ handmade/misc/emacs.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\Emacs\emacs-29.2\bin\runemacs.exe" -q -l w:\handmade\misc\.emacs
---
+++ handmade/misc/shell.bat
@@ -0,0 +1,6 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+set path=w:\handmade\misc;%path%
+doskey em="C:\Program Files\Emacs\emacs-29.2\bin\runemacs.exe"
+
+
